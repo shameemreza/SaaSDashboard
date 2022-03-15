b150ca42a06198f61bddfdfa7a2e8f2c45e0b266
Shameem Reza <codechef@yahoo.com>
2022-03-16 01:11:13 +0600

    Home Completed :fire:

--- SaaSDashboard/View/Home.swift
+++ SaaSDashboard/View/Home.swift
@@ -50,17 +50,110 @@ struct Home: View {
                     } // NOTIFICATION END
                 } // TITLE END
                 
-                // MARK: - USER VIEW
+                // MARK: - ORDER VIEW
                 HStack(spacing: 0) {
+                    OrderProgress(title: "New Order", color: Color("chartColor2"), image: "cart.badge.plus", progress: 68)
                     
+                    OrderProgress(title: "Order Completed", color: Color("roundColor"), image: "clock.badge.checkmark", progress: 72)
                 } // USER VIEW END
+                .padding()
+                .background(Color.white)
+                .cornerRadius(18)
+                .padding(.bottom, -25)
                 
                 // MARK: GRAPH VIEW
                 GraphView(sales: sales)
+                
+                // MARK: - TOP SELLING PRODUCT
+                VStack {
+                    // MARK: SECTION TITLE
+                    HStack {
+                        Text("Top Selling Products")
+                            .font(.callout.bold())
+                        
+                        Spacer()
+                        
+                        Menu {
+                            Button("Day"){}
+                            Button("Week"){}
+                            Button("Month"){}
+                            Button("Year"){}
+                        } label: {
+                            Image("option")
+                                .resizable()
+                                .renderingMode(.template)
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(.primary)
+                        }
+                    } // SECTION TITLE
+                    
+                    // MARK: PRODUCT LIST
+                    HStack(spacing: 15) {
+                        Image(systemName: "bag.fill")
+                            .font(.title2)
+                            .foregroundColor(.pink)
+                            .padding(12)
+                            .background(
+                                Color.gray
+                                    .opacity(0.25)
+                                    .clipShape(Circle())
+                            )
+                        
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Party Bag")
+                                .fontWeight(.bold)
+                            Text("1230 Sales")
+                                .font(.caption2.bold())
+                                .foregroundColor(.gray)
+                        }
+                        
+                        Spacer()
+                        Image(systemName: "cart.fill.badge.plus")
+                            .font(.title2)
+                            .foregroundColor(Color("chartColor2"))
+                    } // END PRODUCT LIST
+                    .padding(.top, 10)
+                } // END TOP SELL
+                .padding()
+                .background(Color.white)
+                .cornerRadius(18)
+                
             }
             .padding()
             
         }
+    }
+    
+    // MARK: USER PROGRESS VIEW
+    @ViewBuilder
+    func OrderProgress(title: String, color: Color, image: String, progress: CGFloat)->some View {
+        HStack {
+            Image(systemName: image)
+                .font(.title2)
+                .foregroundColor(color)
+                .padding(10)
+                .background(
+                    ZStack {
+                        Circle()
+                            .stroke(Color.gray.opacity(0.3), lineWidth: 2)
+                        
+                        Circle()
+                            .trim(from: 0, to: progress / 100)
+                            .stroke(color, lineWidth: 2)
+                    }
+                )
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("\(Int(progress))")
+                    .font(.title2.bold())
+                
+                Text(title)
+                    .font(.caption2.bold())
+                    .foregroundColor(.gray)
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
 
